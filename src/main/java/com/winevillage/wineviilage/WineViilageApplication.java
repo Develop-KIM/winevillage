218cc46e0ac434a3898206603847cf2ca5b3d9aa
Kimdonghwan <kimdonghwan913@gmail.com>
2024-04-17 17:50:52 +0900

    init

---
+++ src/main/java/com/winevillage/wineviilage/WineViilageApplication.java
@@ -0,0 +1,13 @@
+package com.winevillage.wineviilage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineViilageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineViilageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/winevillage/wineviilage/WineViilageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winevillage.wineviilage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineViilageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
