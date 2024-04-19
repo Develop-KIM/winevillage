0a4174d9f34ec8a28ed72e13a1e948ded1a16e93
junuleedk <junulee.dk@gmail.com>
2024-04-19 15:50:54 +0900

    팀오류 수정중

--- src/main/java/com/winevillage/winevillage/MainController.java
+++ src/main/java/com/winevillage/winevillage/MainController.java
@@ -1,4 +1,4 @@
-package com.winevillage.wineviilage;
+package com.winevillage.winevillage;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
