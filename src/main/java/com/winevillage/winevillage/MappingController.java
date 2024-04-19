4d6af28c0b6e5a29d89000bb9593a0ff103f2dc0
Kimdonghwan <kimdonghwan913@gmail.com>
2024-04-19 21:27:23 +0900

    init

--- src/main/java/com/winevillage/winevillage/MappingController.java
+++ src/main/java/com/winevillage/winevillage/MappingController.java
@@ -4,9 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
-
 @Controller
 public class MappingController {
 
