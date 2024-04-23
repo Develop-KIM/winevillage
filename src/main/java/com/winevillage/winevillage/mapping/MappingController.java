8d66198490071c018473b87807518a2a0b4bbb73
Kimdonghwan <kimdonghwan913@gmail.com>
2024-04-23 16:32:31 +0900

    init

--- src/main/java/com/winevillage/winevillage/mapping/MappingController.java
+++ src/main/java/com/winevillage/winevillage/mapping/MappingController.java
@@ -1,4 +1,4 @@
-package com.winevillage.winevillage;
+package com.winevillage.winevillage.mapping;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
