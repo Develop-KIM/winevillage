e6a652ac26465a71e390b47f7e04a1d31c08f28b
JoostLean <jusang2@gmail.com>
2024-04-24 13:15:59 +0900

    20240424

--- src/main/java/com/winevillage/winevillage/cookie/CookieConfig.java
+++ src/main/java/com/winevillage/winevillage/cookie/CookieConfig.java
@@ -3,7 +3,6 @@ package com.winevillage.winevillage.cookie;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,13 +14,6 @@ public class CookieConfig implements WebMvcConfigurer {
 				.addPathPatterns("/**")
 				.excludePathPatterns("/setCookie");
 	}
-	
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-       WebMvcConfigurer.super.addResourceHandlers(registry);
-       registry.addResourceHandler("resources/css/**").addResourceLocations("classpath:/static/")
-       .setCachePeriod(60 * 60 * 24 * 365);
-    }
 
 	@Bean
 	public CookieInterceptor cookieInterceptor() {
